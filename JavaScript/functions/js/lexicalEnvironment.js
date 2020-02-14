f571930b629be8fd0e23e002305cab45c483652d
Vasyl Krupka <krupka.ua@gmail.com>
2020-02-14 10:40:13 +0200

    study Algorithms and data structures

--- JavaScript/functions/js/lexicalEnvironment.js
+++ JavaScript/functions/js/lexicalEnvironment.js
@@ -2,31 +2,33 @@
 
 
 // ===|=== LEXICAL ENVIRONMENT
-var B = 1000;
-var A = 2000;
+// var B = 1000;
+// var A = 2000;
 
-{
-  document.addEventListener("click",
-    function(){
-        console.log(A + " " + " " + C);
-    });
+// const btnClick1 = document.querySelector('.btn-1');
 
-  {
-    var A = 1;
-    let B = 2;
-    const C = 3;
+// {
+//   document.addEventListener("click", 
+//     function(){
+//         console.log(A + " " + " " + C); // 1, 1000, 2000
+//     });
 
-    {
-      // In closure
-      (function() { console.log(A, B, C)}) (); // 
+//   {
+//     var A = 1;
+//     let B = 2;
+//     const C = 3;
 
-      // In timeout callback
-      setTimeout(function(){
-          console.log(A, B, C); // 
-      }, 1000);
-    }
-  }
-}
+//     {
+//       // In closure
+//       (function() { console.log(A, B, C)}) (); // 
+
+//       // In timeout callback
+//       setTimeout(function(){
+//           console.log(A, B, C); // 
+//       }, 1000);
+//     }
+//   }
+// }
 
 
 
