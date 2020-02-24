23780c43b150b862c071cd837a401e666b82a8be
Vasyl Krupka <krupka.ua@gmail.com>
2020-02-24 11:01:58 +0200

    added some example about ingeritance

--- JavaScript/js/object/inheritance.js
+++ JavaScript/js/object/inheritance.js
@@ -387,6 +387,28 @@ alert( fridge.getFood() ); // внутри по-прежнему: котлета
 
 
 
+// =====  EXAMPLE CLASSES
+// class Rectangle{
+//   constructor(height, width) {
+//     this.height = height;
+//     this.width = width;
+//   }
+
+//   get area () {
+//     return this.calcArea;
+//   }
+
+//   get calcArea () {
+//     return this.height * this.width
+//   }
+// }
+
+
+// const square = new Rectangle(10, 10);
+// console.log(square.area); // 100
+
+
+
 
 
 
