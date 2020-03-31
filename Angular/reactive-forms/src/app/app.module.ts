025d586051e52c81b9a275015346ceaebd273f6d
Vasyl Krupka <krupka.ua@gmail.com>
2020-03-31 15:34:49 +0300

    added form: nameEditor

---
+++ Angular/reactive-forms/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule} from "@angular/forms";
+
+import { AppComponent } from './app.component';
+import { NameEditorComponent } from './components/name-editor/name-editor.component';
+import { ProfileEditorComponent } from './components/profile-editor/profile-editor.component';
+
+// @ts-ignore
+@NgModule({
+  declarations: [AppComponent, NameEditorComponent, ProfileEditorComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
