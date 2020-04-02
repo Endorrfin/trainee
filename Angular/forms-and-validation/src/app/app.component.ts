8fc4d180ed79c26b5b06dec345dfd9c1390bdbf3
Vasyl Krupka <krupka.ua@gmail.com>
2020-04-02 11:43:52 +0300

    created validators

--- Angular/forms-and-validation/src/app/app.component.ts
+++ Angular/forms-and-validation/src/app/app.component.ts
@@ -1,11 +1,29 @@
-import {Component} from '@angular/core'
+import {Component, OnInit} from '@angular/core'
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  form: FormGroup // указываем тип переменной
 
+  ngOnInit() {
+    this.form = new FormGroup({ // инициализируем форму
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.maxLength(6)])
+    })
+  }
+
+  submit() {
+    if(this.form.valid) {
+      console.log('Form: ', this.form);
+      const formData = {...this.form.value}
+
+      console.log('Form Data: ', formData);
+    }
+  }
 }
 
--- Angular/forms-and-validation/src/app/app.module.ts
+++ Angular/forms-and-validation/src/app/app.module.ts
@@ -2,19 +2,12 @@ import {BrowserModule} from '@angular/platform-browser'
 import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
-import {FormsModule} from '@angular/forms'
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
-  providers: [
-
-  ],
+  declarations: [AppComponent,],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
