9a1f7461c7693eef61d2a596e9b4a4b6048ec383
Vasyl Krupka <krupka.ua@gmail.com>
2020-04-02 15:12:04 +0300

    created FormGroup address in the form

--- Angular/forms-and-validation/src/app/app.component.ts
+++ Angular/forms-and-validation/src/app/app.component.ts
@@ -13,7 +13,11 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.form = new FormGroup({ // инициализируем форму
       email: new FormControl('', [Validators.email, Validators.required]),
-      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      address: new FormGroup({
+        country: new FormControl('ua'),
+        city: new FormControl('Kyiv', Validators.required)
+      })
     })
   }
 
